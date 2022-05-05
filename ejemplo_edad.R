7114b6829bcd0b1b08df6be715111698dbbcf22a
Ignacio Sarmiento-Barbieri <ignaciomsarmiento@gmail.com>
2022-05-05 18:20:55 -0500

    Cambie el random sampling a 10

--- ejemplo_edad.R
+++ ejemplo_edad.R
@@ -13,10 +13,13 @@ set.seed(10101)
 
 
 
-n_samp<-3
+n_samp<-10
 
 mod_edad <-
   declare_model(N = 100, age = sample(0:80, size = N, replace = TRUE)) +
   declare_inquiry(mean_age = mean(age)) +
   declare_sampling(S = complete_rs(N = N, n = n_samp)) +
   declare_estimator(age ~ 1, model = lm) 
+
+
+
